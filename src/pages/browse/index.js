9d7a8d3f84427af30654bd9d139d2aa578869a76
Matheus Lopes <matheuscla@yahoo.com.br>
2019-05-23 15:25:07 -0300

    Create browse page

--- src/pages/browse/index.js
+++ src/pages/browse/index.js
@@ -1,7 +1,42 @@
 import React from 'react'
 
+import { Container, List, Playlist, Title} from './styles'
+
 const Browse = () => (
-  <h1>Hello World</h1>
+  <Container>
+    <Title>Browse</Title>
+
+    <List>
+      <Playlist>
+        <img
+          src='https://blog.spoongraphics.co.uk/wp-content/uploads/2017/01/thumbnail-2.jpg'
+        />
+      <strong>Rock</strong>
+      <p>Relax while you program listening the best of rock!</p>
+      </Playlist>
+      <Playlist>
+        <img
+          src='https://blog.spoongraphics.co.uk/wp-content/uploads/2017/01/thumbnail-2.jpg'
+        />
+      <strong>Rock</strong>
+      <p>Relax while you program listening the best of rock!</p>
+      </Playlist>
+      <Playlist>
+        <img
+          src='https://blog.spoongraphics.co.uk/wp-content/uploads/2017/01/thumbnail-2.jpg'
+        />
+      <strong>Rock</strong>
+      <p>Relax while you program listening the best of rock!</p>
+      </Playlist>
+      <Playlist>
+        <img
+          src='https://blog.spoongraphics.co.uk/wp-content/uploads/2017/01/thumbnail-2.jpg'
+        />
+      <strong>Rock</strong>
+      <p>Relax while you program listening the best of rock!</p>
+      </Playlist>
+    </List>
+  </Container>
 )
 
 export default Browse
