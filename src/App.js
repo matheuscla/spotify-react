f3c7dc569d2a7bd630a54ac49bf219740e526fe4
Matheus Lopes <matheuscla@yahoo.com.br>
2019-05-23 20:48:08 -0300

    Add and configure redux and sagas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import './config/reactotron'
 import GlobalStyle from './styles/global'
+import store from './store'
 
 import Sidebar from './components/Sidebar'
 import Player from './components/Player'
@@ -13,21 +15,23 @@ import { Wrapper, Container, Content } from './styles/components'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Fragment>
-        <GlobalStyle />
-        <Wrapper>
-          <Container>
-            <Sidebar />
-            <Content>
-              <Header />
-              <Routes />
-            </Content>
-          </Container>
-          <Player />
-        </Wrapper>
-      </Fragment>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Fragment>
+          <GlobalStyle />
+          <Wrapper>
+            <Container>
+              <Sidebar />
+              <Content>
+                <Header />
+                <Routes />
+              </Content>
+            </Container>
+            <Player />
+          </Wrapper>
+        </Fragment>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/config/reactotron.js
+++ src/config/reactotron.js
@@ -1,12 +1,14 @@
-import Reactotron from 'reactotron-react-js'
-import { reactotronRedux } from 'reactotron-redux'
+import Reactotron from 'reactotron-react-js';
+import { reactotronRedux } from 'reactotron-redux';
+import sagaPlugin from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development') {
   const tron = Reactotron.configure()
     .use(reactotronRedux())
-    .connect()
+    .use(sagaPlugin())
+    .connect();
 
-    tron.clear()
+  tron.clear();
 
-    console.tron = tron
+  console.tron = tron;
 }
