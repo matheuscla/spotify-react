e1da7e8cbc661844af93548dfa1acf0101b940a2
Matheus Lopes <matheuscla@yahoo.com.br>
2019-05-23 15:16:25 -0300

    Add react-router-dom and configure routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { Fragment } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 import GlobalStyle from './styles/global'
 
 import Sidebar from './components/Sidebar'
 import Player from './components/Player'
 import Header from './components/Header'
+import Routes from './routes'
 
 import { Wrapper, Container, Content } from './styles/components'
 
 function App() {
   return (
-    <Fragment>
-      <GlobalStyle />
-      <Wrapper>
-        <Container>
-          <Sidebar />
-          <Content>
-            <Header />
-          </Content>
-        </Container>
-        <Player />
-      </Wrapper>
-    </Fragment>
+    <BrowserRouter>
+      <Fragment>
+        <GlobalStyle />
+        <Wrapper>
+          <Container>
+            <Sidebar />
+            <Content>
+              <Header />
+              <Routes />
+            </Content>
+          </Container>
+          <Player />
+        </Wrapper>
+      </Fragment>
+    </BrowserRouter>
   );
 }
 
