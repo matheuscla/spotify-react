7e727b179d6845fb2bd9b2e56bd82afe4abcd327
Matheus Lopes <matheuscla@yahoo.com.br>
2019-05-24 10:48:30 -0300

    Add prev and next feature

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -24,12 +24,13 @@ import ForwardIcon from '../../assets/images/forward.svg'
 import RepeatIcon from '../../assets/images/repeat.svg'
 
 
-const Player = ({ player, play, pause }) => (
+const Player = ({ player, play, pause, next, prev }) => (
   <Container>
     { !!player.currentSong && (
       <Sound
         url={player.currentSong.file}
         playStatus={player.status}
+        onFinishedPlaying={next}
       />
     )}
     <Current>
@@ -50,7 +51,7 @@ const Player = ({ player, play, pause }) => (
         <button>
           <img src={ShuffleIcon} alt='Shuffle' />
         </button>
-        <button>
+        <button onClick={prev}>
           <img src={BackwardIcon} alt='Backward' />
         </button>
         { !!player.currentSong && player.status === Sound.status.PLAYING ? (
@@ -62,7 +63,7 @@ const Player = ({ player, play, pause }) => (
             <img src={PlayIcon} alt='Play' />
           </button>
         ) }
-        <button>
+        <button onClick={next}>
           <img src={ForwardIcon} alt='Forward' />
         </button>
         <button>
@@ -106,7 +107,9 @@ Player.propTypes = ({
     status: PropTypes.string
   }).isRequired,
   play: PropTypes.func.isRequired,
-  pause: PropTypes.func.isRequired
+  pause: PropTypes.func.isRequired,
+  next: PropTypes.func.isRequired,
+  prev: PropTypes.func.isRequired,
 })
 
 const mapStateToProps = state => ({
