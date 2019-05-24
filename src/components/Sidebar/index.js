5b5a5a378ce972c071219980f9ff7f8769518d89
Matheus Lopes <matheuscla@yahoo.com.br>
2019-05-23 21:21:39 -0300

    Ducks for playlists

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,68 +1,75 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Creators as PlaylistActions } from '../../store/ducks/playlists'
 
 import { Container, Nav, NewPlaylist } from './styles'
 
 import addPlaylistIcon from '../../assets/images/add_playlist.svg'
 
-const Sidebar = () => (
-  <Container>
-    <div>
-      <Nav>
-        <li>
-          <a href=''>Browse</a>
-        </li>
-        <li>
-          <a href=''>Radio</a>
-        </li>
-      </Nav>
+class SideBar extends Component {
+  render() {
+    return(
+      <Container>
+        <div>
+          <Nav>
+            <li>
+              <a href=''>Browse</a>
+            </li>
+            <li>
+              <a href=''>Radio</a>
+            </li>
+          </Nav>
 
-      <Nav>
-        <li>
-          <span>YOUR LIBRARY</span>
-        </li>
-        <li>
-          <a href=''>Made for you</a>
-        </li>
-        <li>
-          <a href=''>Recently Played</a>
-        </li>
-        <li>
-          <a href=''>Musics</a>
-        </li>
-        <li>
-          <a href=''>Albums</a>
-        </li>
-        <li>
-          <a href=''>Artists</a>
-        </li>
-        <li>
-          <a href=''>Stations</a>
-        </li>
-        <li>
-          <a href=''>Local Files</a>
-        </li>
-        <li>
-          <a href=''>Videos</a>
-        </li>
-        <li>
-          <a href=''>Podcasts</a>
-        </li>
-      </Nav>
+          <Nav>
+            <li>
+              <span>YOUR LIBRARY</span>
+            </li>
+            <li>
+              <a href=''>Made for you</a>
+            </li>
+            <li>
+              <a href=''>Recently Played</a>
+            </li>
+            <li>
+              <a href=''>Musics</a>
+            </li>
+            <li>
+              <a href=''>Albums</a>
+            </li>
+            <li>
+              <a href=''>Artists</a>
+            </li>
+            <li>
+              <a href=''>Stations</a>
+            </li>
+            <li>
+              <a href=''>Local Files</a>
+            </li>
+            <li>
+              <a href=''>Videos</a>
+            </li>
+            <li>
+              <a href=''>Podcasts</a>
+            </li>
+          </Nav>
 
-      <Nav>
-        <li>
-          <span>PLAYLISTS</span>
-        </li>
-        <li>
-          <a href=''>Best of Rock</a>
-        </li>
-      </Nav>
-    </div>
-    <NewPlaylist>
-      <img src={addPlaylistIcon} alt='Add playlist' />
-      New Playlist
-    </NewPlaylist>
-  </Container>
-)
+          <Nav>
+            <li>
+              <span>PLAYLISTS</span>
+            </li>
+            <li>
+              <a href=''>Best of Rock</a>
+            </li>
+          </Nav>
+        </div>
+        <NewPlaylist>
+          <img src={addPlaylistIcon} alt='Add playlist' />
+          New Playlist
+        </NewPlaylist>
+      </Container>
 
-export default Sidebar
+    )
+  }
+}
+
+export default connect(null, PlaylistActions)(SideBar)
